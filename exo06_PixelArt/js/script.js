49a912d0b04666b6de9cba925757e2a0f9ee5442
Marwa Chanu <m.chanu@interface3.be>
2024-06-06 20:17:27 +0200

    Javascript exo pixel

---
+++ exo06_PixelArt/js/script.js
@@ -0,0 +1,111 @@
+const Jaune = document.querySelector(".Jaune");
+const Bleu = document.querySelector(".Bleu");
+const case1 = document.querySelector("#case1");
+const case2 = document.querySelector("#case2");
+const case3 = document.querySelector("#case3");
+const case4 = document.querySelector("#case4");
+const case5 = document.querySelector("#case5");
+const case6 = document.querySelector("#case6");
+const case7 = document.querySelector("#case7");
+const case8 = document.querySelector("#case8");
+const case9 = document.querySelector("#case9");
+const case10 = document.querySelector("#case10");
+const case11= document.querySelector("#case11");
+const case12= document.querySelector("#case12");
+const case13= document.querySelector("#case13");
+const case14= document.querySelector("#case14");
+const case15= document.querySelector("#case15");
+const case16= document.querySelector("#case16");
+
+
+console.log(case1);
+
+
+let couleurEnCours;
+
+Jaune.addEventListener("click", function(){
+    couleurEnCours = Jaune.textContent
+    console.log(couleurEnCours);
+})
+
+Bleu.addEventListener("click", function(){
+    couleurEnCours = Bleu.textContent
+    console.log(couleurEnCours);
+})
+
+case1.addEventListener("click", function(){
+    case1.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case2.addEventListener("click", function(){
+    case2.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case3.addEventListener("click", function(){
+    case3.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+case4.addEventListener("click", function(){
+    case4.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case5.addEventListener("click", function(){
+    case5.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case6.addEventListener("click", function(){
+    case6.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case7.addEventListener("click", function(){
+    case7.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case8.addEventListener("click", function(){
+    case8.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case9.addEventListener("click", function(){
+    case9.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case10.addEventListener("click", function(){
+    case10.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case11.addEventListener("click", function(){
+    case11.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+case12.addEventListener("click", function(){
+    case12.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case13.addEventListener("click", function(){
+    case13.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case14.addEventListener("click", function(){
+    case14.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+case15.addEventListener("click", function(){
+    case15.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
+
+case16.addEventListener("click", function(){
+    case16.classList.toggle(couleurEnCours)
+    console.log(couleurEnCours);
+})
