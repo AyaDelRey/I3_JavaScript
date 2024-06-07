b076a148a25edb7841b7464f767749a42faea5f2
Marwa Chanu <m.chanu@interface3.be>
2024-06-07 14:37:02 +0200

    Demo JS

---
+++ Demo_recup_DOM/exo_07_somme/main.js
@@ -0,0 +1,4 @@
+/* 
+Faites en sorte que lorsqu'on click sur la margarita,
+l'input somme augmente de son prix (10 euros).
+*/
---
+++ Demo_recup_DOM/types/main.js
@@ -0,0 +1,7 @@
+let maVariable = 5
+maVariable = 'chaine'
+
+//Number
+let varNumber = 5
+varNumber = 5_5
+
